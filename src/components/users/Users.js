b3da1721178ed7a2852f815d5b21792b50396228
charpell <charpellumeh@gmail.com>
2019-08-15 11:26:12 +0100

    githubapi

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,33 +3,10 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 export default class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'charpell',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/21138774?s=460&v=4',
-        html_url: 'https://github.com/Charpell'
-      },
-      {
-        id: '2',
-        login: 'charpell',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/21138774?s=460&v=4',
-        html_url: 'https://github.com/Charpell'
-      },
-      {
-        id: '3',
-        login: 'charpell',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/21138774?s=460&v=4',
-        html_url: 'https://github.com/Charpell'
-      }
-    ]
-  }
-  
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
